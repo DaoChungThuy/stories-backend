855197520d8abb6c3040a284d8b070ebfe4c8050
dthongkhanh <khanhdth_intern@flydinotech.com>
2024-05-28 15:27:28 +0700

    Feat add func login

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Auth\LoginRequest;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    /**
+     * Get a JWT via given credentials.
+     * @param LoginRequest $request
+     * @return HttpResponse
+     */
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+
+        if (!$token = auth()->attempt($credentials)) {
+            return $this->responseErrors(__('auth.unauthorized'), Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = auth()->user();
+
+        if($user->email_verified_at == null) {
+            return $this->responseErrors(__('auth.email_not_verified'), Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->responseSuccess([
+            'user' => $user,
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => JWTAuth::factory()->getTTL() * 60
+        ]);
+    }
+}
