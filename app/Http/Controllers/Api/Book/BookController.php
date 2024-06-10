b3a2e1a1c81b803d29db6310fd636651bb68f7d5
dthongkhanh <khanhdth_intern@flydinotech.com>
2024-06-10 11:37:06 +0700

    Feat: update func AI generate desc

--- app/Http/Controllers/Api/Book/BookController.php
+++ app/Http/Controllers/Api/Book/BookController.php
@@ -3,25 +3,43 @@
 namespace App\Http\Controllers\Api\Book;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Book\GenerateDescRequest;
 use App\Services\Api\Book\GenerateDescBookService;
-use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public function generateBookDesc(Request $request)
+    public function generateBookDesc(GenerateDescRequest $request)
     {
-        $lines = explode("\n", $request->input('old_description'));
-        $filteredLines = array_filter($lines, 'trim');
+        $oldDescription = explode("\n", $request->input('old_description'));
+        $filteredLines = array_filter($oldDescription, 'trim');
         $oldDescription = implode("\n", $filteredLines);
+
         $newDescription = resolve(GenerateDescBookService::class)->generateDesc($oldDescription);
 
-        if (isset($newDescription['candidates'][0]['content']['parts'][0]['text'])) {
-            $result = $newDescription['candidates'][0]['content']['parts'][0]['text'];
-        } else {
-            $result = $oldDescription;
+        if (!$newDescription) {
+            return $this->responseErrors([
+                'message' => __('book.generate_desc_fail'),
+            ]);
         }
-        $result = preg_replace('/##|\*/', '', $result);
 
-        return response()->json($result);
+        $textNewDescription = $oldDescription;
+
+        foreach ($newDescription['candidates'] as $candidate) {
+            if ($candidate['finishReason'] === 'STOP') {
+                foreach ($candidate['content']['parts'] as $part) {
+                    if (isset($part['text'])) {
+                        $textNewDescription = $part['text'];
+                        break 2;
+                    }
+                }
+            }
+        }
+
+        $textNewDescription = preg_replace('/##|\*/', '', $textNewDescription);
+
+        return $this->responseSuccess([
+            'message' => __('book.generate_desc_success'),
+            'data' => $newDescription,
+        ]);
     }
 }
---
+++ lang/en/book.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'generate_desc_success' => 'New description was generated successfully.',
+    'generate_desc_fail' => 'New description was generated failed.',
+];
