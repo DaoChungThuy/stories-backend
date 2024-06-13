b2fa41c3446a86ae3ea29fe19f56c0753bec6117
dthongkhanh <khanhdth_intern@flydinotech.com>
2024-06-13 16:16:44 +0700

    Feat: update url img

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Genre extends Model
 {
@@ -28,5 +30,12 @@ class Genre extends Model
     public function books()
     {
         return $this->hasMany(Book::class);
+    }
+
+    protected function coverImage(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => asset(Storage::url('image/' . $value)),
+        );
     }
 }
