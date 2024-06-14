7891de5f805f28ae66b075a138e2b6afd8acab7b
thuyA3T3 <thuydao13042002@gmail.com>
2024-06-14 10:52:27 +0700

    feate:author_create_story

---
+++ app/Http/Controllers/Api/Book/BookController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api\Book;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Book\CreateBookRequest;
+use App\Http\Resources\Api\Book\BookResource;
+use App\Services\Api\Book\CreateBookService;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function store(CreateBookRequest $createBookRequest)
+    {
+        $book = resolve(CreateBookService::class)->setParams($createBookRequest->validated())->handle();
+
+        if (!$book) {
+            return $this->responseErrors(__('book.create_falsed'));
+        }
+
+        return $this->responseSuccess([
+            'message' =>  __('book.create_success'),
+            'data' => new BookResource($book),
+        ]);
+    }
+}
